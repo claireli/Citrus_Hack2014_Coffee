83d5d46befaf6941f8982f8612bf9f2534c5b4d5
Rachel Law <rlaw1294@gmail.com>
2014-03-01 01:51:47 -0800

    php q&a

---
+++ result.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+YOU CHOSE ANSWER: <?php echo $_POST["q1"]; ?>
+
+
+
+<p>
+THIS IS: <!-- right or wrong -->
+<!-- the if statement goes here-->
+<p> <!-- if wrong, the correct answer is: -->
+</body>
+</html>

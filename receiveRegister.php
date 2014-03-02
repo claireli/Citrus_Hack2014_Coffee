f74aa156266e1abbe33890759185f47083d105cf
Kevin Hsieh <khsie003@ucr.edu>
2014-03-02 12:00:03 -0800

    dfsdfsd

--- receiveRegister.php
+++ receiveRegister.php
@@ -1,3 +1,10 @@
+<html>
+	<head>
+    	<link rel="stylesheet" type="text/css" href="style.css">
+        <title> Question to Server</title>
+    </head>
+	<body>
+
 <?php
 $con=mysqli_connect("localhost","root","","thoughtbox");
 // Check connection
@@ -6,13 +13,24 @@ if (mysqli_connect_errno())
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
 
-$sql="INSERT INTO login (username, password) VALUES ('$_POST[username]','$_POST[password]')";
+$sql="INSERT INTO login (username, password) VALUES ('$_POST[usr]','$_POST[ps]');";
 
 if (!mysqli_query($con,$sql))
   {
   die('Error: ' . mysqli_error($con));
   }
-echo "1 record added";
+//redirect back a page
 
 mysqli_close($con);
 ?>
+
+<a href="HomePage.html">
+<p id="POOP" class="submit"><input type="submit" name="POOP" value="back to home"></a>
+		
+		
+		</div>
+	
+		
+	</form>
+	</body>
+</html>

115793cade329f4f3a298ecdacc35d9965bdcead
Kevin Hsieh <khsie003@ucr.edu>
2014-03-02 11:26:43 -0800

    Feedback

--- FeedbackFormPHP.php
+++ FeedbackFormPHP.php
@@ -21,9 +21,34 @@ $mail = new SendGrid\Email();
 $mail->addTo($input)->
        setFrom('rlaw1294@gmail.com')->
        setSubject('Here are your test results')->
-       setText('We actually dont know either xD');//->
+       setText('Question1: What is one difference between a High Level Language (HLL) like C++ and an Assembly Language (AL)?<br>
+	   		Your Answer:<br>
+	   		Correct Answer: A single HLL statement might compile to many Machine Language instructions, while each AL instruction assembles to a single Machine Language instruction.<br>
+	   		Question2: The LC-3 AL program you wrote in lab1 multiplied a number stored in a register by the constant 6. Which of the following best describes the algorithm used?<br>
+	   		Your Answer:<br>
+	   		Correct Answer: Add the value in the register to itself 6 times<br>
+	   		Question3: A label in assembly language code is:<br>
+	   		Your Answer:<br>
+	   		Correct Answer: A name given to a memory location<br>
+	   		Question4: A pseudo-op in assembly language code (e.g. the LC-s .FILL) is:<br>
+	   		Your Answer:<br>
+	   		Correct Answer: an instruction to the assembler (like a compiler preprocessor directive)<br>');//->
        //setHtml('<strong>Hello World!</strong>');
-	   
+	   /*
+	   		Question1: What is one difference between a High Level Language (HLL) like C++ and an Assembly Language (AL)?<br>
+	   		Your Answer:<br>
+	   		Correct Answer: A single HLL statement might compile to many Machine Language instructions, while each AL instruction assembles to a single Machine Language instruction.<br>
+	   		Question2: The LC-3 AL program you wrote in lab1 multiplied a number stored in a register by the constant 6. Which of the following best describes the algorithm used?<br>
+	   		Your Answer:<br>
+	   		Correct Answer: Add the value in the register to itself 6 times<br>
+	   		Question3: A label in assembly language code is:<br>
+	   		Your Answer:<br>
+	   		Correct Answer: A name given to a memory location<br>
+	   		Question4: A "pseudo-op" in assembly language code (e.g. the LC-s's ".FILL") is:<br>
+	   		Your Answer:<br>
+	   		Correct Answer: an instruction to the assembler (like a compiler preprocessor directive)<br>
+	   		
+	   */
 $result = $sendgrid->send($mail);
 echo "result:";
 echo json_encode($result);   

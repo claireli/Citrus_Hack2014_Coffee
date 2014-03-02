d85eac1df6e997102f3b804e824338c85d3eb19c
claireli <cli024@ucr.edu>
2014-03-02 01:50:06 -0800

    more butts for the code god~

--- questionv2.php
+++ questionv2.php
@@ -1,26 +1,30 @@
 <?php
+session_start();
+
 $con = mysql_connect("localhost","root","claire","thoughtbox");
 if (!$con)
   {
   die('Could not connect: ' . mysql_error());
   }
-
+  
 $sql = "SELECT * FROM thoughtbox.question;";
 if (mysql_query($sql,$con))
   {
   	//loop~ HEAERRRR
   	$result = mysql_query($sql);
   	$recordCount=mysql_num_rows($result);
-  	
-		$qno = mysql_result($result, 0, 'qno');
-		$question = mysql_result($result, 0, 'question');
-		$op1 = mysql_result($result, 0, 'option1');
-		$op2 = mysql_result($result, 0, 'option2');
-		$op3 = mysql_result($result, 0, 'option3');
-		$op4 = mysql_result($result, 0, 'option4');
-		$in_type = mysql_result($result, 0, 'in_type');
-		$in_name = mysql_result($result, 0, 'in_name');
-		$in_value = mysql_result($result, 0, 'in_value');
+  	$x=0;
+		$qno = mysql_result($result, $x, 'qno');
+		$question = mysql_result($result, $x, 'question');
+		$course=mysql_result($result, $x, 'course');
+		$semester=mysql_result($result, $x, 'semester');
+		$op1 = mysql_result($result, $x, 'option1');
+		$op2 = mysql_result($result, $x, 'option2');
+		$op3 = mysql_result($result, $x, 'option3');
+		$op4 = mysql_result($result, $x, 'option4');
+		$in_type = mysql_result($result, $x, 'in_type');
+		$in_name = mysql_result($result, $x, 'in_name');
+		$answer = mysql_result($result, $x, 'answer');
 
 	
 	//the mysql access, the row#, and the field
@@ -30,6 +34,8 @@ else
   echo "Error creating database: " . mysql_error();
   }
 
+$_SESSION['store']=$answer;  
+  
 mysql_close($con);
 ?>
 
@@ -39,12 +45,12 @@ mysql_close($con);
 <html>
     <head>
 	    <link rel="stylesheet" type="text/css" href="style.css">
-        <title> Student Test Question 1</title>
+        <title>Clicker Question</title>
     </head>
     <body><div id="box">
         <div class="container">
             <div class ="login">
-                <font size="3">SUBJECT</font> <br>
+                <font size="3"><?php echo $course; ?> - <?php echo $semester; ?></font> <br>
                 <font size="2">Question #<?php echo $qno; ?>
 				<br></font>
             </div>
@@ -55,11 +61,12 @@ mysql_close($con);
 
             <div class = "login">                        
                 <form action="result.php" method="post">
-                    <input type="radio" name="<?php echo $in_name; ?>" value="a">a. <?php echo $op1; ?> <br>
-                    <input type="radio" name="<?php echo $in_name; ?>" value="b">b. <?php echo $op2; ?> <br>
-                    <input type="radio" name="<?php echo $in_name; ?>" value="c">c. <?php echo $op3; ?> <br>
-                    <input type="radio" name="<?php echo $in_name; ?>" value="d">d. <?php echo $op4; ?>
-                    <p class="submit"><input type="submit" name="commit" value="Submit"></p>
+				
+                    <input type="radio" name="q" value="a">a. <?php echo $op1; ?> <br>
+                    <input type="radio" name="q" value="b">b. <?php echo $op2; ?> <br>
+                    <input type="radio" name="q" value="c">c. <?php echo $op3; ?> <br>
+                    <input type="radio" name="q" value="d">d. <?php echo $op4; ?>
+                    <p class="submit"><input type="submit" name="commit" value="submit"></p>
                 </form>
 				</div>
             </div>                      
